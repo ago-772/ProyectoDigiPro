8ff353564928b53cbeed565a72a086eebc0c9448
ago-772 <agostina.rocio.martin@mi.unc.edu.ar>
2025-11-15 13:42:51 -0300

    Update 4-servo-uart.c

---
+++ new-code/4-servo-uart.c
@@ -0,0 +1,326 @@
+#include "lpc17xx_pinsel.h"
+#include "lpc17xx_gpio.h"
+#include "lpc17xx_timer.h"
+#include "lpc17xx_adc.h"
+#include "lpc17xx_clkpwr.h"
+#include "lpc17xx_nvic.h"
+#include "lpc17xx_uart.h"
+#include <string.h>
+#include <stdint.h>
+#include <stdio.h>
+
+// FUNCION MAPEO SERVO
+static inline uint32_t mapServo(uint16_t val)
+{
+    if(val >=1948 && val<2147) return 1500;
+    if(val <1948) return 1000 + (uint32_t)(((float)val /1947.0f)*500.0f);
+    return 1500 + (uint32_t)(((float)(val-2147)/1948.0f)*500.0f);
+}
+
+// DEFINICIONES SERVOS
+#define SERVO_PIN    9   // P0.9  -> Servo 1
+#define SERVO_PIN2   8   // P0.8  -> Servo 2
+#define SERVO_PIN3   7   // P0.7  -> Servo 3
+#define SERVO_PIN4   6   // P0.6  -> Servo 4 (Timer3)
+
+// ADC
+#define ADC_CH       4   // AD0.4 -> P1.30
+#define ADC_CH5      5   // AD0.5 -> P1.31
+#define ADC_CH2      2   // AD0.2 -> P0.25
+#define ADC_CH3      3   // AD0.3 -> P0.26
+
+// VARIABLES GLOBALES
+volatile uint16_t valorADC1 = 0;
+volatile uint16_t valorADC2 = 0;
+volatile uint16_t valorADC3 = 0;
+volatile uint16_t valorADC4 = 0;
+
+volatile uint32_t pulso  = 1500;
+volatile uint32_t pulso2 = 1500;
+volatile uint32_t pulso3 = 1500;
+volatile uint32_t pulso4 = 1500;
+
+volatile uint32_t counter = 0;
+
+// UART
+void configGPIO_UART(void) {
+    PINSEL_CFG_Type psel_conf;
+
+    psel_conf.Portnum = 0;
+    psel_conf.Pinmode = PINSEL_PINMODE_TRISTATE;
+    psel_conf.OpenDrain = PINSEL_PINMODE_NORMAL;
+
+    psel_conf.Pinnum = 2;    // TXD0
+    psel_conf.Funcnum = 1;
+    PINSEL_ConfigPin(&psel_conf);
+
+    psel_conf.Pinnum = 3;    // RXD0
+    psel_conf.Funcnum = 1;
+    PINSEL_ConfigPin(&psel_conf);
+}
+
+void configUART(void) {
+    UART_CFG_Type uart_cfg;
+    UART_FIFO_CFG_Type fifo_cfg;
+
+    UART_ConfigStructInit(&uart_cfg);
+    UART_Init((LPC_UART_TypeDef *) LPC_UART0, &uart_cfg);
+
+    UART_FIFOConfigStructInit(&fifo_cfg);
+    UART_FIFOConfig((LPC_UART_TypeDef *) LPC_UART0, &fifo_cfg);
+
+    UART_TxCmd((LPC_UART_TypeDef *) LPC_UART0, ENABLE);
+}
+
+// GPIO SERVOS
+void ConfGPIO(void)
+{
+    PINSEL_CFG_Type PinCfg;
+    PinCfg.Portnum = 0;
+    PinCfg.Funcnum = 0;
+    PinCfg.Pinmode = PINSEL_PINMODE_PULLUP;
+    PinCfg.OpenDrain = PINSEL_PINMODE_NORMAL;
+
+    PinCfg.Pinnum = SERVO_PIN;
+    PINSEL_ConfigPin(&PinCfg);
+
+    PinCfg.Pinnum = SERVO_PIN2;
+    PINSEL_ConfigPin(&PinCfg);
+
+    PinCfg.Pinnum = SERVO_PIN3;
+    PINSEL_ConfigPin(&PinCfg);
+
+    PinCfg.Pinnum = SERVO_PIN4;
+    PINSEL_ConfigPin(&PinCfg);
+
+    GPIO_SetDir(0,
+        (1<<SERVO_PIN) |
+        (1<<SERVO_PIN2) |
+        (1<<SERVO_PIN3) |
+        (1<<SERVO_PIN4), 1);
+}
+
+// CONFIG ADC
+void ConfADC(void)
+{
+    PINSEL_CFG_Type PinCfg;
+
+    // AD0.4 (P1.30)
+    PinCfg.Portnum = 1;
+    PinCfg.Pinnum = 30;
+    PinCfg.Funcnum = 3;
+    PinCfg.Pinmode = PINSEL_PINMODE_TRISTATE;
+    PinCfg.OpenDrain = 0;
+    PINSEL_ConfigPin(&PinCfg);
+
+    // AD0.5 (P1.31)
+    PinCfg.Pinnum = 31;
+    PINSEL_ConfigPin(&PinCfg);
+
+    // AD0.2 (P0.25)
+    PinCfg.Portnum = 0;
+    PinCfg.Pinnum = 25;
+    PinCfg.Funcnum = 1;
+    PINSEL_ConfigPin(&PinCfg);
+
+    // AD0.3 (P0.26)
+    PinCfg.Pinnum = 26;
+    PINSEL_ConfigPin(&PinCfg);
+
+    ADC_Init(LPC_ADC, 1000);
+    ADC_BurstCmd(LPC_ADC, ENABLE);
+
+    ADC_ChannelCmd(LPC_ADC, ADC_CH, ENABLE);
+    ADC_ChannelCmd(LPC_ADC, ADC_CH5, ENABLE);
+    ADC_ChannelCmd(LPC_ADC, ADC_CH2, ENABLE);
+    ADC_ChannelCmd(LPC_ADC, ADC_CH3, ENABLE);
+
+    ADC_IntConfig(LPC_ADC, ADC_CH, ENABLE);
+    ADC_IntConfig(LPC_ADC, ADC_CH5, ENABLE);
+    ADC_IntConfig(LPC_ADC, ADC_CH2, ENABLE);
+    ADC_IntConfig(LPC_ADC, ADC_CH3, ENABLE);
+
+    ADC_StartCmd(LPC_ADC, ADC_START_CONTINUOUS);
+    NVIC_EnableIRQ(ADC_IRQn);
+}
+
+// ADC IRQ
+void ADC_IRQHandler(void)
+{
+    if (ADC_ChannelGetStatus(LPC_ADC, ADC_CH, 1)) {
+        valorADC1 = ADC_ChannelGetData(LPC_ADC, ADC_CH);
+        pulso  = mapServo(valorADC1);
+        TIM_UpdateMatchValue(LPC_TIM2, 1, pulso);
+    }
+
+    if (ADC_ChannelGetStatus(LPC_ADC, ADC_CH5, 1)) {
+        valorADC2 = ADC_ChannelGetData(LPC_ADC, ADC_CH5);
+        pulso2 = mapServo(valorADC2);
+        TIM_UpdateMatchValue(LPC_TIM2, 2, pulso2);
+    }
+
+    if (ADC_ChannelGetStatus(LPC_ADC, ADC_CH2, 1)) {
+        valorADC3 = ADC_ChannelGetData(LPC_ADC, ADC_CH2);
+        pulso3 = mapServo(valorADC3);
+        TIM_UpdateMatchValue(LPC_TIM2, 3, pulso3);
+    }
+
+    if (ADC_ChannelGetStatus(LPC_ADC, ADC_CH3, 1)) {
+        valorADC4 = ADC_ChannelGetData(LPC_ADC, ADC_CH3);
+        pulso4 = mapServo(valorADC4);
+        TIM_UpdateMatchValue(LPC_TIM3, 1, pulso4);   // MR1 TIMER3
+    }
+}
+
+// TIMER2 (SERVOS 1–2–3)
+void ConfTIMER2(void)
+{
+    TIM_TIMERCFG_Type timerCfg;
+    timerCfg.PrescaleOption = TIM_PRESCALE_USVAL;
+    timerCfg.PrescaleValue = 1;
+    TIM_Init(LPC_TIM2, TIM_TIMER_MODE, &timerCfg);
+
+    // MR0 (periodo)
+    TIM_MATCHCFG_Type match0;
+    match0.MatchChannel = 0;
+    match0.IntOnMatch = ENABLE;
+    match0.ResetOnMatch = ENABLE;
+    match0.StopOnMatch = DISABLE;
+    match0.ExtMatchOutputType = TIM_EXTMATCH_NOTHING;
+    match0.MatchValue = 20000;
+    TIM_ConfigMatch(LPC_TIM2, &match0);
+
+    // MR1 servo1
+    TIM_MATCHCFG_Type match1;
+    match1.MatchChannel = 1;
+    match1.IntOnMatch = ENABLE;
+    match1.ResetOnMatch = DISABLE;
+    match1.StopOnMatch = DISABLE;
+    match1.ExtMatchOutputType = TIM_EXTMATCH_NOTHING;
+    match1.MatchValue = 1500;
+    TIM_ConfigMatch(LPC_TIM2, &match1);
+
+    // MR2 servo2
+    TIM_MATCHCFG_Type match2;
+    match2.MatchChannel = 2;
+    match2.IntOnMatch = ENABLE;
+    match2.ResetOnMatch = DISABLE;
+    match2.StopOnMatch = DISABLE;
+    match2.ExtMatchOutputType = TIM_EXTMATCH_NOTHING;
+    match2.MatchValue = 1500;
+    TIM_ConfigMatch(LPC_TIM2, &match2);
+
+    // MR3 servo3
+    TIM_MATCHCFG_Type match3;
+    match3.MatchChannel = 3;
+    match3.IntOnMatch = ENABLE;
+    match3.ResetOnMatch = DISABLE;
+    match3.StopOnMatch = DISABLE;
+    match3.ExtMatchOutputType = TIM_EXTMATCH_NOTHING;
+    match3.MatchValue = 1500;
+    TIM_ConfigMatch(LPC_TIM2, &match3);
+
+    NVIC_EnableIRQ(TIMER2_IRQn);
+    TIM_Cmd(LPC_TIM2, ENABLE);
+}
+
+// TIMER3 (SERVO 4)
+void ConfTIMER3(void)
+{
+    TIM_TIMERCFG_Type timerCfg;
+    timerCfg.PrescaleOption = TIM_PRESCALE_USVAL;
+    timerCfg.PrescaleValue = 1;
+    TIM_Init(LPC_TIM3, TIM_TIMER_MODE, &timerCfg);
+
+    // MR0 – período
+    TIM_MATCHCFG_Type match0;
+    match0.MatchChannel = 0;
+    match0.IntOnMatch = ENABLE;
+    match0.ResetOnMatch = ENABLE;
+    match0.StopOnMatch = DISABLE;
+    match0.ExtMatchOutputType = TIM_EXTMATCH_NOTHING;
+    match0.MatchValue = 20000;
+    TIM_ConfigMatch(LPC_TIM3, &match0);
+
+    // MR1 – servo 4
+    TIM_MATCHCFG_Type match1;
+    match1.MatchChannel = 1;
+    match1.IntOnMatch = ENABLE;
+    match1.ResetOnMatch = DISABLE;
+    match1.StopOnMatch = DISABLE;
+    match1.ExtMatchOutputType = TIM_EXTMATCH_NOTHING;
+    match1.MatchValue = 1500;
+    TIM_ConfigMatch(LPC_TIM3, &match1);
+
+    NVIC_EnableIRQ(TIMER3_IRQn);
+    TIM_Cmd(LPC_TIM3, ENABLE);
+}
+
+// TIMER2 IRQ
+void TIMER2_IRQHandler(void)
+{
+    if (TIM_GetIntStatus(LPC_TIM2, TIM_MR0_INT))
+    {
+        counter++;
+
+        if (counter >= 50)
+        {
+            char msg[160];
+            sprintf(msg, "Pulsos: %u %u %u %u\r\n", pulso, pulso2, pulso3, pulso4);
+            UART_Send((LPC_UART_TypeDef*)LPC_UART0, (uint8_t*)msg, strlen(msg), BLOCKING);
+            counter = 0;
+        }
+
+        TIM_ClearIntPending(LPC_TIM2, TIM_MR0_INT);
+
+        GPIO_SetValue(0, 1<<SERVO_PIN);
+        GPIO_SetValue(0, 1<<SERVO_PIN2);
+        GPIO_SetValue(0, 1<<SERVO_PIN3);
+    }
+
+    if (TIM_GetIntStatus(LPC_TIM2, TIM_MR1_INT)) {
+        TIM_ClearIntPending(LPC_TIM2, TIM_MR1_INT);
+        GPIO_ClearValue(0, 1<<SERVO_PIN);
+    }
+
+    if (TIM_GetIntStatus(LPC_TIM2, TIM_MR2_INT)) {
+        TIM_ClearIntPending(LPC_TIM2, TIM_MR2_INT);
+        GPIO_ClearValue(0, 1<<SERVO_PIN2);
+    }
+
+    if (TIM_GetIntStatus(LPC_TIM2, TIM_MR3_INT)) {
+        TIM_ClearIntPending(LPC_TIM2, TIM_MR3_INT);
+        GPIO_ClearValue(0, 1<<SERVO_PIN3);
+    }
+}
+
+// TIMER3 IRQ
+void TIMER3_IRQHandler(void)
+{
+    if (TIM_GetIntStatus(LPC_TIM3, TIM_MR0_INT))
+    {
+        TIM_ClearIntPending(LPC_TIM3, TIM_MR0_INT);
+        GPIO_SetValue(0, 1<<SERVO_PIN4);
+    }
+
+    if (TIM_GetIntStatus(LPC_TIM3, TIM_MR1_INT))
+    {
+        TIM_ClearIntPending(LPC_TIM3, TIM_MR1_INT);
+        GPIO_ClearValue(0, 1<<SERVO_PIN4);
+    }
+}
+
+// MAIN
+int main(void)
+{
+    ConfGPIO();
+    ConfADC();
+    ConfTIMER2();
+    ConfTIMER3();
+    configGPIO_UART();
+    configUART();
+
+    while (1)
+    {
+    }
+}
